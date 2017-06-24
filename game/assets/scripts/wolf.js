f2f02a53240d063a3e9fc2509182598c3ca04196
HarpoNeu <harponeu@gmail.com>
2017-06-24 16:24:03 -0400

    Wolf fear towards barking

--- game/assets/scripts/wolf.js
+++ game/assets/scripts/wolf.js
@@ -6,6 +6,13 @@ var WOLF_STATE_IDLE;
 var WOLF_STATE_ATTACKING;
 var WOLF_STATE_HUNTING;
 
+var WOLF_STRESS_MIN = 0;
+var WOLF_STRESS_MAX = 15;
+var WOLF_STRESS_THRESHOLD = 10.0;
+var WOLF_STRESS_RUN_SPEED = 2.0;
+var WOLF_STRESS_RANGE_CONTRIB_PER_SECOND = 2.0;
+var WOLF_STRESS_COOLDOWN_PER_SECOND = WOLF_STRESS_RANGE_CONTRIB_PER_SECOND * 0.15
+
 var wolfs = [];
 
 function wolf_init() 
@@ -24,6 +31,7 @@ function wolf_create(wolfPos)
         position: new Vector2(wolfPos),
         state: WOLF_STATE_IDLE,
         size: WOLF_SIZE,
+        stress: WOLF_STRESS_MIN,
         target: sheeps[0],
         renderFn: wolf_render,
     };
@@ -38,7 +46,7 @@ function wolf_render(wolf)
 
 function wolf_spawn() 
 {
-    var wolfTryPos = Random.randCircle(MAP_CENTER, TILE_SIZE * 15)
+    var wolfTryPos = Random.randCircle(MAP_CENTER, TILE_SIZE * MAP_SIZE)
     var wolf = wolf_create(wolfTryPos)
     wolfs.push(wolf);
     pushers.push(wolf);
@@ -56,6 +64,11 @@ function wolfs_update(dt)
 
 function wolf_targetAcquisition(wolf, sheep)
 {
+    if (!sheep.dead && wolf.target.dead)
+    {
+        wolf.target = sheep; 
+        return; 
+    }
     var distance = Vector2.distance(sheep.position, wolf.position);
     var currentTargetDistance = Vector2.distance(wolf.target.position, wolf.position);
     if (currentTargetDistance > distance)
@@ -89,9 +102,42 @@ function wolf_moveToward(wolf, speed, dt)
 
 function wolf_update(wolf, dt)
 {
-    for (var i = 0; i < sheeps.length; i++) 
+    for (var i = 0; i < dogs.length; ++i) {
+        var dog = dogs[i];
+
+        var distance = Vector2.distance(wolf.position, dog.position);
+
+          if (distance < TILE_SIZE * 1.6) {
+            wolf.stress = Math.min(wolf.stress + (dog.fearFactor * WOLF_STRESS_RANGE_CONTRIB_PER_SECOND * dt), WOLF_STRESS_MAX);
+            if (wolf.stress > WOLF_STRESS_THRESHOLD)
+            {
+                var factor = new Vector2(TILE_SIZE);
+                wolf.targetPosition = wolf.position.add(factor.mul(wolf.position.sub(dog.position)));
+
+                // if (beeeeeCoolDown < 0)
+                // {
+                //     playSound("SFX_sheep_alarm_" + Random.randInt(1, 8) + ".wav", .25);
+                //     beeeeeCoolDown = 1;
+                // }
+            }
+         }
+    }
+
+    if (wolf.stress < WOLF_STRESS_THRESHOLD)
     {
-        wolf_targetAcquisition(wolf, sheeps[i]);
-    }    
-    wolf_moveToward(wolf, WOLF_SPEED, dt);
+        for (var i = 0; i < sheeps.length; i++) 
+        {
+            wolf_targetAcquisition(wolf, sheeps[i]);
+        }    
+        wolf_moveToward(wolf, WOLF_SPEED, dt);
+    }
+    else
+    {
+        var dir = wolf.targetPosition.sub(wolf.position).normalize();
+        wolf.position = wolf.position.add(dir.mul(WOLF_SPEED * dt));
+        print("Wolf is stressed!"); 
+    }
+    wolf.stress = Math.max(wolf.stress-(WOLF_STRESS_COOLDOWN_PER_SECOND*dt), WOLF_STRESS_MIN);
+
+
 }
